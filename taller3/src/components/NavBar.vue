6df428be97902a5528ce10ab6bde4feef33283c5
Yhiazer <b.urrea01@ufromail.cl>
2024-05-27 17:00:54 -0400

    Se crea repositorio

---
+++ taller3/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+      <div class="container">
+        <nav>
+          <a @click="goToPrincipal">Volver</a>
+        </nav>
+      </div>
+    </header>
+</template>
+<script>
+export default {
+  methods: {
+    goToPrincipal() {
+      this.$router.push({ name: 'principal' });
+    }
+  }
+}
+</script>
---
+++ taller3/src/views/PrincipalView.vue
@@ -0,0 +1,108 @@
+<script>
+export default {
+  methods: {
+    goToLogin() {
+      this.$router.push({ name: 'login' });
+    }
+  }
+}
+</script>
+
+<template>
+
+  <body>
+    <header>
+      <div class="container">
+        <nav>
+          <a href="#historia">Historia</a>
+          <a href="#tutorial">Tutorial</a>
+          <a href="#final">Jugar</a>
+        </nav>
+      </div>
+    </header>
+
+    <section id="inicio">
+      <img src="@/media/logo.png" alt="logo.png">
+      <button @click="goToLogin">Jugar</button>
+    </section>
+
+    <section id="historia">
+      <div class="container">
+        <div class="texto">
+          <h2>Historia</h2>
+          <p>Durante la pelea final en Elbaf, Barba Negra se vio acorralado por los Sombreros de Paja, ante tal
+            desesperación consigue lo impensable,
+            el despertar de la Yami Yami no Mi junto con la Gura Gura no Mi, desatando un poder colosal que rompio todas
+            las barreras del sentido común.
+            <br> Con tal poder Barba Negra hizo colapsar multiples realidades, trayendo consigo impresionantes
+            personajes de
+            realiades distantes con el solo
+            objetivo de obtener aun mas poder y volverse imparable ante la amenaza que representan los Sombreros de
+            Paja.
+            <br>¿Podrá Luffy y sus compañeros
+            hacerle frente a esta nueva amenaza, conseguira más aliados o los derrotara antes de que Barba Negra lo haga
+            para extraerle su poder?
+          </p>
+        </div>
+        <div class="img-container">
+          <video src="@/media/dbzop_mute.mp4" autoplay="true" muted="true" loop="true" width="720px"
+            poster="@/media/luffyvsgoku.png"></video>
+        </div>
+      </div>
+      <div class="cont">
+        <button>+ Informacion</button>
+      </div>
+    </section>
+
+    <section id="tutorial">
+      <div class="container">
+        <h2><span class="color-acento">Antes</span> de Jugar</h2>
+        <div class="informacion">
+          <div class="carta">
+            <h3 class="color-acento">Controles</h3>
+            <p><span style="font-weight: bold;">Jugador </span> 1 <br>
+              <span style="font-weight: bold;">Movimiento:</span> Flechas de dirección<br>
+              <span style="font-weight: bold;">Teleportación:</span> Num 0<br>
+              <span style="font-weight: bold;">Jugador </span> 2 <br>
+              <span style="font-weight: bold;">Movimiento:</span> W, A, S y D<br>
+              <span style="font-weight: bold;">Teleportación:</span> C<br>
+            </p>
+          </div>
+          <div class="carta">
+            <h3 class="color-acento">Mecanicas</h3>
+            <p>Cada cierto periodo de tiempo el atributo
+              de ataque de
+              ambos peleadores subira de manera aleatoria, haciendolos mas poderosos, pero existe la posibilidad de que
+              falles tu ataque. <br>
+              Tambien existe la habilidad de teletransporte, aunque no abuses de ello ya que vaciara tu barra de
+              accion, dejandote vulnerable.</p>
+          </div>
+          <div class="carta">
+            <h3 class="color-acento">Personajes</h3>
+            <p>La grieta dimensional que genero Barba Negra atrajo a peleadores de distintos universos, tales como
+              Dragon Ball Z, Ben 10, hasta
+              incluso Hot Wheels, sin olvidar a la trio monstruoso de los Sombreros de Paja. ¿Quien sera capaz de
+              alzarce ante Barba Negra y
+              quedarse con la victoria?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="final">
+      <h2>¿Listo para Jugar?</h2>
+      <button>Jugar</button>
+    </section>
+
+    <footer>
+      <div class="container">
+        <p>&copy; All rigths reserved</p>
+        <p>Aclaración: No somos los autores ni dueños de ningun material audiovisual, y, por ende, todo material usado
+          es solo para
+          fines demostrativos.
+        </p>
+      </div>
+    </footer>
+  </body>
+</template>
